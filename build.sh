b08e6e1406eeebca937865e37ac77a5750c053a5
martlandh <HarryMartland@Rentalcars.com>
2017-03-14 21:09:41 +0000

    added $GOPATH build debug

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 pwd
 ls
+echo $GOPATH
 docker run -v $GOPATH:/go golang:alpine /bin/sh -c 'cd /go/src/github.com/harryemartland/orderly-badger && go get -v -d && go build'
 docker build -t orderly-badger .
