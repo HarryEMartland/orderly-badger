aa8abb8fd4dc5058e057cdfaf30990579a2b0e0d
martlandh <HarryMartland@Rentalcars.com>
2017-03-14 20:53:21 +0000

    changed build to use $GOPATH

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker run -v ~/go:/go golang:alpine /bin/sh -c 'cd /go/src/github.com/harryemartland/orderly-badger; go get -v -d; go build'
+docker run -v $GOPATH:/go golang:alpine /bin/sh -c 'cd /go/src/github.com/harryemartland/orderly-badger; go get -v -d; go build'
 docker build -t orderly-badger .
