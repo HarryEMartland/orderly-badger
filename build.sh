5f70ff185bba3b7d8065c717bb927bed3055171f
martlandh <HarryMartland@Rentalcars.com>
2017-03-14 21:14:02 +0000

    changed case of folder in build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 pwd
 ls
 echo $GOPATH
-docker run -v $GOPATH:/go golang:alpine /bin/sh -c 'cd /go/src/github.com/harryemartland/orderly-badger && go get -v -d && go build'
+docker run -v $GOPATH:/go golang:alpine /bin/sh -c 'cd /go/src/github.com/HarryEMartland/orderly-badger && go get -v -d && go build'
 docker build -t orderly-badger .
